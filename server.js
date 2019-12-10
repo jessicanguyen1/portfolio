feefa79ea16d98ce26e7025bdccd06737b8a544c
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-12-10 15:40:46 -0800

    created routes for each path and added active class for nav bar

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ app.set("views", "views");
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { path: "/" });
 });
 
 app.get("/portfolio", (req, res, next) => {
-  res.render("portfolio");
+  res.render("portfolio", { path: "/portfolio" });
+});
+
+app.get("/contact", (req, res, next) => {
+  res.render("contact", { path: "/contact" });
+});
+
+app.get("/resume", (req, res, next) => {
+  res.render("resume", { path: "/resume" });
 });
 
 app.listen(PORT, () => {
