af688f2a8f77deea46fd6b88b17d7b07243dd122
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-12-16 21:49:34 -0800

    moved favicon to public folder

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const favicon = require("serve-favicon");
+const path = require("path");
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.get("/", (req, res, next) => {
   res.render("index", { path: "/", title: "JN" });
